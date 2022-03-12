27bbc1701b73e0b456f0c5c692bfc740033c3250
ssohit111 <ssohit111@gmail.com>
2022-03-12 12:04:26 +0530

    bg color Bottom nav solved

--- App.js
+++ App.js
@@ -1,92 +1,8 @@
 import React from "react";
-import { Ionicons } from "@expo/vector-icons";
-import { createAppContainer } from "react-navigation";
-import { createBottomTabNavigator } from "react-navigation-tabs";
-
-import HomeScreen from "./screens/HomeScreen"
-import StartScreen from "./screens/StartScreen";
-import SettingScreen from "./screens/SettingScreen";
-import ReportScreen from './screens/ReportScreen'
-
 
 import { Themecontext } from "./Themecontroller";
 
-const TabNavigator = createBottomTabNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarOptions: {
-        activeTintColor: "#006600",
-      },
-      tabBarIcon: (tabInfo) => {
-        return (
-          <Ionicons
-            name="md-home"
-            size={24}
-            color={tabInfo.focused ? "#006600" : "#8e8e93"}
-          />
-        );
-      },
-    },
-  },
-  StartScreen: {
-    screen: StartScreen,
-    navigationOptions: {
-      tabBarLabel: "Start",
-      tabBarOptions: {
-        activeTintColor: "#006600",
-      },
-      tabBarIcon: (tabInfo) => {
-        return (
-          <Ionicons
-            name="chevron-forward-circle-outline"
-            size={24}
-            color={tabInfo.focused ? "#006600" : "#8e8e93"}
-          />
-        );
-      },
-    },
-  },
-  Report: {
-    screen: ReportScreen,
-    navigationOptions: {
-      tabBarLabel: "Report",
-      tabBarOptions: {
-        activeTintColor: "#006600",
-      },
-      tabBarIcon: (tabInfo) => {
-        return (
-          <Ionicons
-            name="stats-chart-outline"
-            size={24}
-            color={tabInfo.focused ? "#006600" : "#8e8e93"}
-          />
-        );
-      },
-    },
-  },
-  Setting: {
-    screen: SettingScreen,
-    navigationOptions: {
-      tabBarLabel: "Settings",
-      tabBarOptions: {
-        activeTintColor: "#006600",
-      },
-      tabBarIcon: (tabInfo) => {
-        return (
-          <Ionicons
-            name="md-settings-outline"
-            size={24}
-            color={tabInfo.focused ? "#006600" : "#8e8e93"}
-          />
-        );
-      },
-    },
-  },
-});
-
-const Navigator = createAppContainer(TabNavigator);
+import { Navigator, NavigatorDark } from "./components/TabNavigators";
 
 export default function App() {
   const [Dark, setDark] = React.useState(true);
@@ -94,8 +10,8 @@ export default function App() {
     setDark(new_Theme)
   }
   return (
-    <Themecontext.Provider value={{ Dark, changeTheme }}>
-      <Navigator />
+    <Themecontext.Provider value={{ Dark, changeTheme }} >
+      {Dark === false ? (<Navigator />) : (<NavigatorDark />)}
     </Themecontext.Provider>
   );
 }
---
+++ components/TabNavigators.js
@@ -0,0 +1,174 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import HomeScreen from '../screens/HomeScreen'
+import ReportScreen from '../screens/ReportScreen'
+import SettingScreen from '../screens/SettingScreen'
+import StartScreen from '../screens/StartScreen'
+
+const TabNavigator = createBottomTabNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            tabBarLabel: 'Home',
+            tabBarOptions: {
+                activeTintColor: "#006600",
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="md-home"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    StartScreen: {
+        screen: StartScreen,
+        navigationOptions: {
+            tabBarLabel: "Start",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="chevron-forward-circle-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    Report: {
+        screen: ReportScreen,
+        navigationOptions: {
+            tabBarLabel: "Report",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="stats-chart-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    Setting: {
+        screen: SettingScreen,
+        navigationOptions: {
+            tabBarLabel: "Settings",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="md-settings-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+});
+
+const TabNavigatorDark = createBottomTabNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            tabBarLabel: 'Home',
+            tabBarOptions: {
+                activeTintColor: "#006600",
+                style: {
+                    backgroundColor: 'black'
+                }
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="md-home"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    StartScreen: {
+        screen: StartScreen,
+        navigationOptions: {
+            tabBarLabel: "Start",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+                style: {
+                    backgroundColor: 'black'
+                }
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="chevron-forward-circle-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    Report: {
+        screen: ReportScreen,
+        navigationOptions: {
+            tabBarLabel: "Report",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+                style: {
+                    backgroundColor: 'black'
+                }
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="stats-chart-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+    Setting: {
+        screen: SettingScreen,
+        navigationOptions: {
+            tabBarLabel: "Settings",
+            tabBarOptions: {
+                activeTintColor: "#006600",
+                style: {
+                    backgroundColor: 'black'
+                }
+            },
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="md-settings-outline"
+                        size={24}
+                        color={tabInfo.focused ? "#006600" : "#8e8e93"}
+                    />
+                );
+            },
+        },
+    },
+});
+
+export const Navigator = createAppContainer(TabNavigator);
+export const NavigatorDark = createAppContainer(TabNavigatorDark);
